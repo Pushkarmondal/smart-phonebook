072eb4a7f1ef3c9b9d182bbd26316e0485d2a793
Pushkar Mondal <nishitm060@gmail.com>
2025-07-21 22:03:57 +0530

    Now time for query engine

---
+++ query.ts
@@ -0,0 +1,14 @@
+import { GoogleGenAI } from "@google/genai";
+
+// The client gets the API key from the environment variable `GEMINI_API_KEY`.
+const ai = new GoogleGenAI({});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Explain what is callback is js?",
+  });
+  console.log(response.text);
+}
+
+main();
