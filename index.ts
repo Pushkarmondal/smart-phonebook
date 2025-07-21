e7f8455e7f8fec417fa4a12cb57ab4b0886512e5
Pushkar Mondal <nishitm060@gmail.com>
2025-07-21 18:18:02 +0530

    more routes added 🚏

--- index.ts
+++ index.ts
@@ -199,7 +199,106 @@ app.get("/api/contact/get-by-id/:id", async(req, res) => {
     }
 })
 
+// PUT /contacts/:id
+app.put("/api/contact/update/:id", async(req, res) => {
+    try {
+        const {id} = req.params;
+        const contact = await prisma.contact.update({
+            where: {
+                id
+            },
+            data: req.body,
+            select: {
+                id: true,
+                name: true,
+                type: true,
+                phone: true,
+                email: true,
+                location: true,
+                tags: true,
+                addedBy: {
+                    select: {
+                        id: true,
+                        name: true,
+                        email: true
+                    }
+                },
+                relationships: {
+                    select: {
+                        user: true,
+                        relation: true,
+                        visibility: true,
+                        notes: true
+                    }
+                },
+                createdAt: true,
+            }
+        })
+        return res.status(200).json({message: "Contact updated successfully!", contact});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({error: "Internal server error in Update Contact route!"});
+    }
+})
 
+// DELETE /contacts/:id
+app.delete("/api/contact/delete/:id", async(req, res) => {
+    try {
+        const {id} = req.params;
+        const contact = await prisma.contact.delete({
+            where: {
+                id
+            }
+        })
+        return res.status(200).json({message: "Contact deleted successfully!", contact});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({error: "Internal server error in Delete Contact route!"});
+    }
+})
+
+// GET /contacts/search?q=
+app.get("/api/contact/search?q=", async(req, res) => {
+    try {
+        const {q} = req.query;
+        const contacts = await prisma.contact.findMany({
+            where: {
+                name: {
+                    contains: q as string
+                }
+            },
+            select: {
+                id: true,
+                name: true,
+                type: true,
+                phone: true,
+                email: true,
+                location: true,
+                tags: true,
+                addedBy: {
+                    select: {
+                        id: true,
+                        name: true,
+                        email: true
+                    }
+                },
+                relationships: {
+                    select: {
+                        user: true,
+                        relation: true,
+                        visibility: true,
+                        notes: true
+                    }
+                },
+                createdAt: true,
+            }
+        })
+        return res.status(200).json({message: "Contacts fetched successfully!", contacts});
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({error: "Internal server error in Search Contact route!"});
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
