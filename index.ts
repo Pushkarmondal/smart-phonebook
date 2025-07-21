2c5eabbaebb4c09e8da115d88fc8a3018a2417db
Pushkar Mondal <nishitm060@gmail.com>
2025-07-21 19:56:33 +0530

    relationship routes

--- index.ts
+++ index.ts
@@ -64,7 +64,7 @@ app.post("/api/auth/login", async (req, res) => {
         if(!await bcrypt.compare(password, user.password)) {
             return res.status(401).json({error: "Invalid password!"});
         }
-        const token = jsonwebtoken.sign({id: user.id}, "secret", {expiresIn: "1h"});
+        const token = jsonwebtoken.sign({id: user.id}, "secret", {expiresIn: "48h"});
         const { password: _, ...userWithoutPassword } = user;
         return res.status(200).json({message: "User logged in successfully!", userWithoutPassword, token});
     } catch (error) {
@@ -258,7 +258,7 @@ app.delete("/api/contact/delete/:id", async(req, res) => {
 })
 
 // GET /contacts/search?q=
-app.get("/api/contact/search?q=", async(req, res) => {
+app.get("/api/contact/search", async(req, res) => {
     try {
         const {q} = req.query;
         const contacts = await prisma.contact.findMany({
@@ -299,6 +299,147 @@ app.get("/api/contact/search?q=", async(req, res) => {
         return res.status(500).json({error: "Internal server error in Search Contact route!"});
     }
 })
+
+// POST /relationships
+app.post("/api/relationship/create", async(req, res) => {
+    try {
+        const {userId, contactId, relation, visibility, notes} = req.body;
+        if(!userId || !contactId || !relation) {
+            return res.status(400).json({error: "User ID, Contact ID and Relation are required!"});
+        }
+        const relationship = await prisma.relationship.create({
+            data: {
+                user: {
+                    connect: {
+                        id: userId
+                    }
+                },
+                contact: {
+                    connect: {
+                        id: contactId
+                    }
+                },
+                relation,
+                visibility,
+                notes
+            }
+        })
+        return res.status(201).json({message: "Relationship created successfully!", relationship});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: "Internal server error in Create Relationship route!"});
+    }
+})
+
+// GET /relationships
+app.get("/api/relationship/get-all", async(req, res) => {
+    try {
+        const relationships = await prisma.relationship.findMany({
+            select: {
+                id: true,
+                user: {
+                    select: {
+                        id: true,
+                        name: true,
+                        email: true
+                    }
+                },
+                contact: {
+                    select: {
+                        id: true,
+                        name: true,
+                        type: true,
+                        phone: true,
+                        email: true,
+                        location: true,
+                        tags: true,
+                        addedBy: {
+                            select: {
+                                id: true,
+                                name: true,
+                                email: true
+                            }
+                        },
+                        relationships: {
+                            select: {
+                                user: true,
+                                relation: true,
+                                visibility: true,
+                                notes: true
+                            }
+                        },
+                        createdAt: true,
+                    }
+                },
+                relation: true,
+                visibility: true,
+                notes: true,
+                createdAt: true,
+            }
+        })
+        return res.status(200).json({message: "Relationships fetched successfully!", relationships});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: "Internal server error in Get All Relationships route!"});
+    }
+});
+
+// GET /relationships/:id
+app.get("/api/relationship/get-by-id/:id", async(req, res) => {
+    try {
+        const {id} = req.params;
+        const relationship = await prisma.relationship.findUnique({
+            where: {
+                id
+            },
+            select: {
+                id: true,
+                user: {
+                    select: {
+                        id: true,
+                        name: true,
+                        email: true
+                    }
+                },
+                contact: {
+                    select: {
+                        id: true,
+                        name: true,
+                        type: true,
+                        phone: true,
+                        email: true,
+                        location: true,
+                        tags: true,
+                        addedBy: {
+                            select: {
+                                id: true,
+                                name: true,
+                                email: true
+                            }
+                        },
+                        relationships: {
+                            select: {
+                                user: true,
+                                relation: true,
+                                visibility: true,
+                                notes: true
+                            }
+                        },
+                        createdAt: true,
+                    }
+                },
+                relation: true,
+                visibility: true,
+                notes: true,
+                createdAt: true,
+            }
+        })
+        return res.status(200).json({message: "Relationship fetched successfully!", relationship});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: "Internal server error in Get Relationship by ID route!"});
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
